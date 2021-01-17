5592896d518d09d45944d8d065475c09d6fafd02
Irene <Irene@Irene-HP>
2021-01-17 12:59:36 +0100

    start esercizio

--- src/esercizio3/Aggiusteria.java
+++ src/esercizio3/Aggiusteria.java
@@ -5,13 +5,66 @@
  */
 package esercizio3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Irene
  */
 public class Aggiusteria {
     
-    Apparecchio[] a1 = new Apparecchio[10];
+  private final List <Apparecchio> elencoApparecchi;
+  private int id =0;
+
+    public Aggiusteria(int id) {
+        this.id = id;
+        elencoApparecchi= new ArrayList<>(id);
+        inizializza();
+    }
+
+    public int getId() {
+        return id;
+    }
+     public void inserisci(Apparecchio a) {
+        if (riparazioniInCorso() == -1) {
+            throw new AggiusteriaException("Lavoro pieno...");
+        }
+        elencoApparecchi.set(riparazioniInCorso(), a);
+    }
+    
+     public Apparecchio elimina(int id) {
+        if (id >= this.id || elencoApparecchi.get(id) == null) {
+            throw new AggiusteriaException("L'id non esiste oppure è vuoto");
+        }
+        Apparecchio a = elencoApparecchi.get(id);
+        elencoApparecchi.set(id, null);
+        return a;
+    }
+     
+     public void stampaSituazione() {
+        for (int id = 0; id < elencoApparecchi.size(); id++) {
+            if (elencoApparecchi.get(id) != null) {
+                System.out.println("--------------- ID " + id + "------------------");
+                System.out.println(elencoApparecchi.get(id));
+                System.out.println("---------------------------------");
+                System.out.println();
+            }
+
+        }
+    }
+
+  
+    private void inizializza() {
+        for(int i=0; i<this.id; i++){
+           elencoApparecchi.add(null);
+        }
+    }
+    
+    private int riparazioniInCorso() {
+        return elencoApparecchi.indexOf(null);
+    }
+    
     
     
 }
---
+++ src/esercizio3/AggiusteriaException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizio3;
+
+/**
+ *
+ * @author Irene
+ */
+public class AggiusteriaException extends RuntimeException {
+
+    public AggiusteriaException(String message) {
+        super(message);
+        
+    }
+    
+}
